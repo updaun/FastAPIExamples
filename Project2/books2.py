835a7581887605ff6bdf0491eca4f45ed581f644
updaun <updauney@daum.net>
2024-06-01 23:00:59 +0900

    [feat] project2 using pydantic

---
+++ Project2/books2.py
@@ -0,0 +1,58 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+    published_date: int
+
+    def __init__(self, id, title, author, description, rating, published_date):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+        self.published_date = published_date
+
+
+class BookRequest(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
+    published_date: int
+
+
+BOOKS = [
+    Book(1, "Computer Science Pro", "codingwithroby", "A very nice book!", 5, 2030),
+    Book(2, "Be Fast with FastAPI", "codingwithroby", "A great book!", 5, 2030),
+    Book(3, "Master Endpoints", "codingwithroby", "A awesome book!", 5, 2029),
+    Book(4, "HP1", "Author 1", "Book Description", 2, 2028),
+    Book(5, "HP2", "Author 2", "Book Description", 3, 2027),
+    Book(6, "HP3", "Author 3", "Book Description", 1, 2026),
+]
+
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+
+@app.post("/create-book")
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(find_book_id(new_book))
+
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book
